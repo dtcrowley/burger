6de0a3e2bd1a57a958d17015bcc004fdb6a8032e
dtcrowley <crowleyd1988@gmail.com>
2018-04-04 21:38:39 -0400

    MVC format establish, connection.js, burger_controller.js, npm installations all done

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers ("burger_name", devoured)
+VALUES ("Mushroom Swiss", false);
+
+INSERT INTO burgers ("burger_name", devoured)
+VALUES ("Baconator", true);
+
+INSERT INTO burgers ("burger_name", devoured)
+VALUES ("Veggie", false);
